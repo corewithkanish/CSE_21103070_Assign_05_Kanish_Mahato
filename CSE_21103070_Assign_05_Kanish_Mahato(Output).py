615271c939800fc41e8b5cbec7b914f08fd1648f
corewithkanish <72293910+corewithkanish@users.noreply.github.com>
2022-03-05 20:07:49 +0530

    Add files via upload

---
+++ CSE_21103070_Assign_05_Kanish_Mahato(Output).py
@@ -0,0 +1,86 @@
+# Windows PowerShell
+# Copyright (C) Microsoft Corporation. All rights reserved.
+
+# Try the new cross-platform PowerShell https://aka.ms/pscore6
+
+# Warning: PowerShell detected that you might be using a screen reader and has disabled PSReadLine for compatibility purposes. If you want to re-enable it, run 'Import-Module
+# PSReadLine'.
+
+# PS A:\Python> & C:/Users/Asus/AppData/Local/Programs/Python/Python310/python.exe "a:/Python/CSE_21103070_Assign_05_Kanish_Mahato(Source Code).py"
+# >>>>>>>>>>>>>>>>>Q5<<<<<<<<<<<<<<<<<<<<
+
+# How many number you are entering
+# 5
+# 12
+# 34
+# 21
+# 124
+# 12
+# [12, 34, 21, 124, 12]
+# a)part:==>
+# Ascending Order:==>
+# [12, 12, 21, 34, 124]
+# b)part:==>
+# Enter number you want to find it:--
+# 12
+# Element is present at index 0
+# Number you want to ask
+# 12
+# c)part:--
+# The number 12 has occured 2 times
+# ------------------------++++++---------------------------
+# PS A:\Python> & C:/Users/Asus/AppData/Local/Programs/Python/Python310/python.exe "a:/Python/CSE_21103070_Assign_05_Kanish_Mahato(Source Code).py"
+# >>>>>>>>>>>>>>>>>Q1<<<<<<<<<<<<<<<<<<<<
+
+# ------------------------++++++---------------------------
+# >>>>>>>>>>>>>>>>>>>>>>>Q2<<<<<<<<<<<<<<<<<<<<<<<<<<
+# -----------------------++++++------------------------------------
+# >>>>>>>>>>>>>>>>>Q3<<<<<<<<<<<<<<<<<<<<
+
+# ------------------------++++++---------------------------
+# >>>>>>>>>>>>>>>>>>>>>Q4<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# Enter no. of students entering their input
+# 7
+# Enter marks of students:--
+# 9
+# Enter marks of students:--
+# 8
+# Enter marks of students:--
+# 7
+# Enter marks of students:--
+# 6
+# Enter marks of students:--
+# 5
+# Enter marks of students:--
+# 10
+# Enter marks of students:--
+# 4
+# Sortinng list
+# [4, 5, 6, 7, 8, 9, 10]
+# ------------------------++++++---------------------------
+# >>>>>>>>>>>>>>>>>Q5<<<<<<<<<<<<<<<<<<<<
+
+# How many number you are entering
+# 7
+# 12
+# 13
+# 14
+# 15
+# 16
+# 17
+# 18
+# [12, 13, 14, 15, 16, 17, 18]
+# a)part:==>
+# Ascending Order:==>
+# [12, 13, 14, 15, 16, 17, 18]
+# b)part:==>
+# Enter number you want to find it:--
+# 12
+# Element is present at index 0
+# Number you want to ask
+# 12
+# c)part:--
+# The number 12 has occured 1 times
+# ------------------------++++++---------------------------
+# PS A:\Python>
